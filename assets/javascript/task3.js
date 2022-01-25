2aaa694fce74a2f85edbce4f7438047d3b4b85f0
Drew Fischer <drew.fischer@vtsd.com>
2022-01-25 09:25:14 -0500

    12/15/21

---
+++ assets/javascript/task3.js
@@ -0,0 +1,8 @@
+/* Put code for the task below */
+function changeDirections(){
+    var task3 = document.getElementById("directions3");
+    task3.style.diplay="none";
+
+    var newText = document.getElementById("newText");
+    newText.style.display="block";
+}
